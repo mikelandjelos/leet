b43f57b2d3e0f00dcc0117d38fe5f486c1d365d0
mikelandjelos <mihajlo.madic@gmail.com>
2025-03-15 18:00:54 +0100

    1 problems solved - Leet1380

--- Cisco/13LuckyNumbersInAMatrix.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
-        min_rows = set(map(min, matrix))
-        max_cols = set(map(max, zip(*matrix)))
-        return list(min_rows & max_cols)
